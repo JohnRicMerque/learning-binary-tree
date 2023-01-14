8a041523914a4b64c156035c510e96c3a43eed03
John Ric Merque <johnricmer@gmail.com>
2023-01-14 10:07:12 +0800

    made similar code in left for adding child in the right subtree

--- binary_tree.py
+++ binary_tree.py
@@ -12,9 +12,13 @@ class BinarySearchTreeNode:
         if data < self.data: # if the value is less than the value of current node
             # add data in left subtree
             if self.left: # if left element has value
-                self.left.add_child(data) # call the add_child method of that value 
+                self.left.add_child(data) # call the add_child method of that value that will lead to a recursion
             else: # if empty
                 self.left = BinarySearchTreeNode(data) # assign data to the left subtree
 
         else: # if data is greater
             # add data in the right subtree
+            if self.right: # if right element has value
+                self.right.add_child(data) # call the add_child method of that value that will lead to a recursion
+            else: # if empty
+                self.right = BinarySearchTreeNode(data) # assign data to the right subtree
