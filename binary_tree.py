8910caf3d180f0f91952d9e334b73134bf11fc17
John Ric Merque <johnricmer@gmail.com>
2023-01-14 10:03:01 +0800

    adds data in left subtree when it's empty and if not perform a recursion on the left value using its add_child function

--- binary_tree.py
+++ binary_tree.py
@@ -9,3 +9,12 @@ class BinarySearchTreeNode:
         if data == self.data: # if the value already exists 
             return # then no need to add anything because binary search tree cannot have duplicate elements, hence return only
         
+        if data < self.data: # if the value is less than the value of current node
+            # add data in left subtree
+            if self.left: # if left element has value
+                self.left.add_child(data) # call the add_child method of that value 
+            else: # if empty
+                self.left = BinarySearchTreeNode(data) # assign data to the left subtree
+
+        else: # if data is greater
+            # add data in the right subtree
