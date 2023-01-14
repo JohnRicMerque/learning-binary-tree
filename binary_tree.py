79aaa8860c6538b4375e05a5fb448a25806bd45a
John Ric Merque <johnricmer@gmail.com>
2023-01-14 09:37:05 +0800

    defining a binary search tree node class

---
+++ binary_tree.py
@@ -0,0 +1,7 @@
+class BinarySearchTreeNode:
+    # defining binary search tree node class
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
