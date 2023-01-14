a3df9fc6bb884966de3dab0ff4e25ec0f610510b
John Ric Merque <johnricmer@gmail.com>
2023-01-14 14:27:15 +0800

    added new file with array of fullname, imported modules and built bst

---
+++ fullname_binary_tree.py
@@ -0,0 +1,4 @@
+from binary_tree import BinarySearchTreeNode, build_tree
+                
+fullname = ["J", "O", "H", "N", "R", "I", "C", "C", "A", "T", "A", "P", "A", "N", "G", "M", "E", "R", "Q", "U", "E"]
+numbers_tree = build_tree(fullname)
